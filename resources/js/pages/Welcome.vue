250d221c92604b705a6b2b748f21804a0e12c1dc
Alejandro <alejandropino88@gmail.com>
2026-01-20 11:52:53 +0100

    Remove booking display section from Welcome.vue to simplify the component layout.

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -21,13 +21,5 @@ onMounted(() => {
     <div class="flex flex-col items-center gap-4 m-4">
         <h1 class="text-2xl font-bold">{{ t('welcome.title') }} {{ store.testUser }}</h1>
         <p class="text-gray-500">{{ t('welcome.subtitle') }}</p>
-
-        <div class="grid grid-cols-2 gap-4 w-[40%]">
-            <div v-for="booking in store.bookings" :key="booking.id">
-                <h2 class="text-lg font-bold">{{ booking.id }}</h2>
-                <p class="text-gray-500">{{ moment(booking.checkin_at).format('DD/MM/YYYY') }}</p>
-                <p class="text-gray-500">{{ moment(booking.checkout_at).format('DD/MM/YYYY') }}</p>
-            </div>
-        </div>
     </div>
 </template>
